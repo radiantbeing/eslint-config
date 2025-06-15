76cbf9aca872016973b2f42d3b5182fdf29b2103
김인화 <radiantbeing99@gmail.com>
2025-06-15 15:14:39 +0900

    `needsFreeze` 함수 제거
    JSLint 규칙과 동일하게 동작하도록 변경함.

--- src/rules/freeze-default-export.js
+++ src/rules/freeze-default-export.js
@@ -1,48 +1,32 @@
+function isObjectFreezeCall(node) {
+    return (
+        node.type === "CallExpression"
+        && node.callee.type === "MemberExpression"
+        && node.callee.object.name === "Object"
+        && node.callee.property.name === "freeze"
+    );
+}
+
 export default Object.freeze({
     create(context) {
-        function isObjectFreezeCall(node) {
-            return (
-                node.type === "CallExpression"
-                && node.callee.type === "MemberExpression"
-                && node.callee.object.name === "Object"
-                && node.callee.property.name === "freeze"
-            );
-        }
-
-        function needsFreeze(node) {
-            return (
-                node.type === "ObjectExpression"
-                || node.type === "ArrayExpression"
-                || node.type === "FunctionExpression"
-                || node.type === "ArrowFunctionExpression"
-                || node.type === "FunctionDeclaration"
-                || (
-                    node.type === "CallExpression"
-                    && node.callee.name === "defineConfig"
-                )
-            );
-        }
-
         return {
             ExportDefaultDeclaration(node) {
                 const {declaration} = node;
                 if (isObjectFreezeCall(declaration)) {
                     return;
                 }
-                if (needsFreeze(declaration)) {
-                    context.report({
-                        fix(fixer) {
-                            const sourceCode = context.getSourceCode();
-                            const text = sourceCode.getText(declaration);
-                            return fixer.replaceText(
-                                declaration,
-                                `Object.freeze(${text})`
-                            );
-                        },
-                        messageId: "requireFreeze",
-                        node: declaration
-                    });
-                }
+                context.report({
+                    fix(fixer) {
+                        const sourceCode = context.getSourceCode();
+                        const text = sourceCode.getText(declaration);
+                        return fixer.replaceText(
+                            declaration,
+                            `Object.freeze(${text})`
+                        );
+                    },
+                    messageId: "requireFreeze",
+                    node: declaration
+                });
             }
         };
     },
